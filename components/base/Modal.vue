75ec7b29bf925877f617e37a0994703a3b3b9687
benyakir <benyakir.horowitz@gmail.com>
2023-09-07 11:09:00 -0600

    begint o setup for multiselect and modal

---
+++ components/base/Modal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { open } = defineProps<{ open: boolean }>()
+defineOptions({
+  inheritAttrs: false
+})
+</script>
+
+<template>
+  <Teleport to="body">
+    <dialog
+      :open="open"
+      v-bind="$attrs"
+    >
+      <slot></slot>
+    </dialog>
+  </Teleport>
+</template>
+
