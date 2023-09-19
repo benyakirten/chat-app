127015385ed0588f45138bc5111ba71d59b762a1
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2023-09-19 16:11:26 -0600

    make grid single-column

--- components/account/Section.vue
+++ components/account/Section.vue
@@ -20,7 +20,8 @@ defineProps<{ title: string }>()
 
 .group {
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  /* grid-template-columns: repeat(2, 1fr); */
+  grid-template-columns: 1fr;
   gap: 4rem;
 
   @media (width < 600px) {
