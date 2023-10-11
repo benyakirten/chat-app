daac62d920ece967e44e951e2c22c839b5a8b011
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2023-10-11 12:08:16 -0600

    move axios middleware to server plugin

--- plugins/environment.server.ts
+++ plugins/environment.server.ts
@@ -1,4 +1,10 @@
+import axios from 'axios'
+
 export default defineNuxtPlugin((app) => {
+  if (axios.defaults.baseURL) {
+    return
+  }
+
   const { apiUrl, wsUrl } = app.$config.public
 
   let missingEnvViariables: string[] = []
@@ -12,4 +18,7 @@ export default defineNuxtPlugin((app) => {
   if (missingEnvViariables.length > 0) {
     throw new Error(`${missingEnvViariables.join(', ')} environment variable(s) must be defined`)
   }
+
+  axios.defaults.baseURL = apiUrl
+  axios.defaults.validateStatus = () => true
 })
