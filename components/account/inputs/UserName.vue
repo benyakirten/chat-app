736fc6d8420d5ae01c45d26d47f7d184475b6857
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2023-10-10 21:41:29 -0600

    begin to modify user name input

--- components/account/inputs/UserName.vue
+++ components/account/inputs/UserName.vue
@@ -3,12 +3,14 @@ const props = defineProps<{ name: string }>()
 const userStore = useUsersStore()
 
 const userName = ref(props.name)
-const { debouncer } = useDebounce((val: string) => !!val && userStore.setUserName(val), 1200)
-watch(userName, (val) => val && debouncer(val))
+function updateUserName() {
+  // TODO: Add sockets first
+}
 </script>
 
 <template>
   <GeneralInputText v-model="userName" style="max-width: 25rem" placeholder="Choose a username">
     <template #label><AccountInputsLabel>Display Name</AccountInputsLabel></template>
   </GeneralInputText>
+  <BaseButton :disabled="userName === userStore.userMe?.name" @click="updateUserName"> Update user name </BaseButton>
 </template>
